09ead6b432fd1fca03275062411836d5fb68b9cc
daan <2087884@talnet.nl>
2020-02-10 10:03:41 +0100

    band af

--- band.php
+++ band.php
@@ -1,17 +1,19 @@
 <?php
 
 
-    $totaal = 0;
-    $prijslijst = array("Citizen of Glass"=>4.50, "Night"=>9, "New Eyes"=>5, "Strange Trails"=>10);
+    $totaal = "0";
+    $prijslijst = array("Citizen of Glass" => "4.50", "Night" => "9", "New Eyes" => "5", "Strange Trails" => "10");
     $getal = count($prijslijst);
 
+    echo("het albumoverzicht:".PHP_EOL);
+
     foreach($prijslijst as $prijs => $var){
-        echo("$prijs kost $var".PHP_EOL);
-        $totaal + $var;
+        echo("$prijs kost €$var".PHP_EOL);
+        $totaal = $totaal + $var;
     }
 
-    echo("de totaal prijs is $totaal".PHP_EOL);
-    echo("het gemiddelde is $totaal/$getal");
+    echo("de totaal prijs is €$totaal".PHP_EOL);
+    echo("het gemiddelde is €".($totaal / $getal));
 
 
 
